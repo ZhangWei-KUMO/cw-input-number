d29b275bf6ca1eb322b2cbe02fa2237c8841ca56
ZhangWei-KUMO <kanseefoil@gmail.com>
2020-03-11 20:09:42 +0800

    initial code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+
 import classNames from 'classnames';
 import KeyCode from 'rc-util/lib/KeyCode';
 import InputHandler from './InputHandler';
